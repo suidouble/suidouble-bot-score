512f16ce000d7f4bf543049146bbc2018882dee8
jeka <jeka911@gmail.com>
2023-07-28 02:52:10 +0300

    score page

---
+++ frontend/src/pages/Score.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div></div>
+  
+  </template>
+  
+  <script>
+  
+  export default {
+      name: 'Score Page',
+      path: '/score/:color',
+      props: {
+      },
+      components: {
+        //   ColorHome,
+      },
+      data() {
+          return {
+          }
+      },
+      watch: {
+      },
+      methods: {
+      },
+      computed: {
+      },
+      beforeMount() {
+      },
+      mounted() {
+        document.location = '/';
+      },
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style>
+      h6 {
+          font-size: 16px;
+          font-weight: bold;
+          margin: 0 0 16px 0;
+      }
+  
+      h5 {
+          margin: 0 0 16px 0;
+      }
+  
+  </style>
+  
+  
